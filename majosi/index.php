c7c1a4bc0a77d9d681db42b09b4e708b0dd34a24
Carlos Castro <61478783+carloskastro@users.noreply.github.com>
2022-05-24 14:00:57 -0500

    Add files via upload

---
+++ majosi/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es-CO">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Carlos Andrés</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	<header>
+		<h1>Algoritmos en PHP</h1>
+	</header>
+	<section>	
+		<nav>
+			<h6>Menú</h6>
+			<ul>
+				<li><a href="retos/reto1.php">Reto 1</a></li>
+				<li><a href="retos/reto2.php">Reto 2</a></li>
+				<li><a href="retos/reto3.php">Reto 3</a></li>
+				<li><a href="retos/reto4.php">Reto 4</a></li>
+				<li><a href="retos/reto5.php">Reto 5</a></li>
+				<li><a href="retos/reto6.php">Reto 6</a></li>
+				<li><a href="retos/reto7.php">Reto 7</a></li>
+				<li><a href="retos/reto8.php">Reto 8</a></li>
+			</ul>
+		</nav>
+		<article>
+
+		</article>
+	</section>
+	<footer>
+		<h6>Copyright Carlos Castro</h6>
+	</footer>
+</body>
+</html>
