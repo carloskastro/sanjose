753c2779d2644b14b5f053143bd61e5bfa7c9afc
Carlos Castro <61478783+carloskastro@users.noreply.github.com>
2024-07-04 15:45:17 -0500

    Delete app directory

--- app/imagen.php
+++
@@ -1,9 +0,0 @@
-/*----subir imagen al servidor--------*/
-    $temp->bindParam(':imagen',$ruta);
-    $nombreimg=$_FILES['imagen']['name'];
-    $archivo=$_FILES['imagen']['tmp_name'];
-    $ruta='ad_img/'.$nombreimg;
-    move_uploaded_file($archivo, $ruta);
-/*----subir imagen al servidor--------*/
-
-<form action="" class="form-horizontal" method="post" enctype="multipart/form-data">
